703e52e4817c975a8c090bd10b53aadcd7c772d9
pietro.abate <pietro.abate@anu.edu.au>
2007-04-04 07:26:37 +0200

    cli changes 3.6
    darcs-hash:20070404052637-5b3eb-b12c9f67ac584ce14744acf223211d032d697def

--- cli/logic.ml
+++ cli/logic.ml
@@ -1,17 +1,20 @@
 
-open Datatype
+module Make(T : TwbSet.ValType)(H : TwbHash.ValType) = struct
 
-let __strategy : (Strategy.node -> Strategy.m) option ref = ref None
-let __matchpatt : (Type.bt -> string) option ref = ref None
-let __inputparser : (string -> Type.bt list) option ref = ref None
-let __printer : (Basictype.formula -> string) option ref = ref None
-let __substitute : (Basictype.formula -> Basictype.formula -> 
-    Basictype.formula -> Basictype.formula) option ref = ref None
-let __simplification : (Basictype.formula -> Basictype.formula -> 
-    Basictype.formula ) option ref = ref None
-let __history_list : (string * Type.t * history_type) list option ref = ref None
-let __pp : (Type.bt list -> Type.bt list) option ref = ref None
-let __neg : (Type.bt list -> Type.bt list) option ref = ref None
-let __exit : (Variable.store list -> string) option ref = ref None
-let __options : (Arg.key * Arg.spec * Arg.doc) list option ref = ref None
-let __use_cache : bool ref = ref false
+    module DataType = DataType.Make(T)(H)
+    open DataType
+
+    let __strategy : (Strategy.node -> Strategy.m) option ref = ref None
+    let __matchpatt : (T.t -> string) option ref = ref None
+    let __inputparser : (string -> T.t list) option ref = ref None
+    let __printer : (T.t -> string) option ref = ref None
+    let __substitute : (T.t -> T.t -> T.t -> T.t) option ref = ref None
+    let __simplification : (T.t -> T.t -> T.t ) option ref = ref None
+    let __history_list : (string * H.t * history_type) list option ref = ref None
+    let __pp : (T.t list -> T.t list) option ref = ref None
+    let __neg : (T.t list -> T.t list) option ref = ref None
+    let __exit : (Variable.store list -> string) option ref = ref None
+    let __options : (Arg.key * Arg.spec * Arg.doc) list option ref = ref None
+    let __use_cache : bool ref = ref false
+
+end
